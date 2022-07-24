15437972acfe2b17c45dfafa05979e3da22654b6
Ivan Andrianov <i.a.andrianov@gmail.com>
2022-07-24 16:48:02 +0300

    postgres connection and simple select query

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,7 +1,37 @@
-import pq.PQlibVersion
+import kotlinx.cinterop.toKString
+import pq.*
 
 public fun main() {
-    print("pq version: ")
-    print(PQlibVersion())
-    println()
+    println("pq version: ${PQlibVersion()}")
+
+    val conn = PQconnectdb("postgresql://kefir:kefir@localhost:8570/kefir")
+        ?: throw RuntimeException("conn couldn't be established")
+    try {
+        println("conn status: ${PQstatus(conn)}")
+        if (PQstatus(conn) != ConnStatusType.CONNECTION_OK) {
+            throw RuntimeException("error during establishing conn: ${PQerrorMessage(conn)?.toKString()}")
+        }
+        println("DB version: ${PQserverVersion(conn)}")
+        val result = PQexec(conn, "SELECT 1+2")
+            ?: throw RuntimeException("query couldn't be executed")
+        try {
+            println("query status: ${PQresultStatus(result)}")
+            if (PQresultStatus(result) != PGRES_TUPLES_OK) {
+                throw RuntimeException("error during running query: ${PQerrorMessage(conn)?.toKString()}")
+            }
+            println("number of rows: #${PQntuples(result)}")
+            println("number of columns: #${PQnfields(result)}")
+            for (row in 0 until PQntuples(result)) {
+                for (column in 0 until PQnfields(result)) {
+                    println("value [$row, $column]: ${PQgetvalue(result, row, column)?.toKString()}")
+                    println("type [$row, $column]: ${PQparamtype(result, column)}")
+                    println("is null [$row, $column]: ${PQgetisnull(result, row, column)}")
+                }
+            }
+        } finally {
+            PQclear(result)
+        }
+    } finally {
+        PQfinish(conn)
+    }
 }
